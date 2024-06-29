e338cf2aae4346284146dbc129697897b78518f8
Vishwas1516 <jmvishwas8@gmail.com>
2024-06-29 12:08:40 +0530

    Add files via upload

---
+++ llsearch.py
@@ -0,0 +1,49 @@
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_end(self, data):
+        if not self.head:
+            self.head = Node(data)
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = Node(data)
+
+    def search(self, data):
+        current = self.head
+        while current:
+            if current.data == data:
+                return True
+            current = current.next
+        return False
+
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.data, end = " ")
+            current = current.next
+        print()
+
+# Create a new linked list
+ll = LinkedList()
+
+# Insert some nodes
+ll.insert_at_end(1)
+ll.insert_at_end(2)
+ll.insert_at_end(3)
+ll.insert_at_end(4)
+ll.insert_at_end(5)
+
+# Search for a node
+print(ll.search(3))
+print(ll.search(6))  
+
+# Print the linked list
+ll.print_list()  
